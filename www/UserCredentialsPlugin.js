73bd07b187e57895747e52777e209fc8468713f4
Asaf Novak <asaf.novak@MacMini-AsafN.local>
2017-05-07 11:22:48 +0300

    added content to UserCredentialsPlugin.js

--- www/UserCredentialsPlugin.js
+++ www/UserCredentialsPlugin.js
@@ -1,1 +1,19 @@
-UserCredentialsPlugin.js
+var exec = require("cordova/exec");
+module.exports = {
+  setUserCredentials: function (successCallback, errorCallback, _userName, _deviceType, _deviceID) {
+        if (errorCallback == null) { errorCallback = function () { } }
+        if (typeof errorCallback != "function") {
+            console.log("UserCredentialsPlugin.setUserCredentials failure: failure parameter not a function");
+            return
+        }
+
+        if (typeof successCallback != "function") {
+            console.log("UserCredentialsPlugin.setUserCredentials failure: success callback parameter must be a function");
+            return
+        }
+        
+         exec(successCallback, errorCallback, "UserCredentialsPlugin", "setUserCredentials", [_userName, _deviceType, _deviceID]);
+  }
+  
+
+};
